0fd73cc4ee7b3940dc6f056a423bf85e868cf755
Diego <dsimog01@estudiantes.unileon.es>
2023-08-09 10:51:08 +0200

    Fix web3signer API endpoint

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 VALIDATOR_PORT=3500
-WEB3SIGNER_API="http://web3signer.web3signer-${NETWORK}.dappnode:9000"
 WALLET_DIR="/root/.eth2validators"
 
 # Copy auth-token in runtime to the prysm token dir
@@ -15,14 +14,13 @@ exec -c validator \
   --monitoring-host 0.0.0.0 \
   --beacon-rpc-provider="beacon-chain.prysm-lukso.dappnode:4000" \
   --beacon-rpc-gateway-provider="beacon-chain.prysm-lukso.dappnode:3500" \
-  --validators-external-signer-url="$WEB3SIGNER_API" \
+  --validators-external-signer-url="http://web3signer.web3signer-lukso.dappnode:9000" \
   --rpc-host 0.0.0.0 \
   --grpc-gateway-host=0.0.0.0 \
   --grpc-gateway-port="$VALIDATOR_PORT" \
   --grpc-gateway-corsdomain=http://0.0.0.0:"$VALIDATOR_PORT" \
   --graffiti="$GRAFFITI" \
   --suggested-fee-recipient="${FEE_RECIPIENT_ADDRESS}" \
-  --web \
   --accept-terms-of-use \
   --enable-doppelganger \
   --verbosity $LOG_VERBOSITY \
